c3fd3be5881b37c371afe0038bc8f47578f0d117
Barata Estevao <barataestevaomario@gmail.com>
2020-04-27 18:46:05 -0700

    BEM VINDO

---
+++ src/main/java/com/example/LES_1/Les1Application.java
@@ -0,0 +1,13 @@
+package com.example.LES_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Les1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Les1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LES_1/Les1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LES_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Les1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
